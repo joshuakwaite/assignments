60e64c9ab713b66abf5432a322fe8f089a781092
Josh Waite <kalewaite@gmail.com>
2017-01-16 07:59:57 -0700

    updated mario projected

---
+++ projects/goomba-pest-control/index.js
@@ -0,0 +1,16 @@
+//function totalCoins (x,y,z) {
+//    var sumCoins = x.length + y.length + z.length
+//}
+//
+//console.log(totalCoins(3,11,4));
+
+
+//document.getElementById("button").addEventListener("click", totalCoins);
+function getElement(){
+    var goomba = document.getElementById("goombaInput").value;
+    
+    
+}
+document.getElementById("button").addEventListener("click",getElement)
+
+console.log(goomba)
---
+++ projects/goomba-pest-control/test.js
@@ -0,0 +1,25 @@
+//function missingChar(str, n){ 
+//	var newString = ""
+//    for (var i = 0; i < str.length; i++) {
+//      	if (str[i] != n) {
+//            console.log(str[i])
+//       	 	newString = newString + str[i];
+//        } else {
+//         continue;
+//             
+//        }
+//    }
+//                       return newString;
+//}
+//                                   
+//console.log(missingChar("goomba","o"));
+
+var startHi = function(str){     
+    console.log(str.substr(0,2))
+  if (str.substr(0,2 == "hi")) {
+      return true;
+      } else {
+      return false;
+      }
+}
+console.log(startHi("hi"));
