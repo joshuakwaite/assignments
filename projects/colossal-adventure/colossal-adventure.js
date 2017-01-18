3065a2daa613a9413bcc3b8e33969991cf3ad7b6
Josh Waite <kalewaite@gmail.com>
2017-01-18 13:20:59 -0700

    updated projects

--- projects/colossal-adventure/colossal-adventure.js
+++ projects/colossal-adventure/colossal-adventure.js
@@ -1,32 +1,42 @@
 var readline = require("readline-sync");
 
+var menu = ["Access Inventory", "Continue Walking", "Quit"]
+var inventory = ["Healing Potion", "Throwing Knife", ]
+var randomEnemy = ["Werewolf", "Vampire Underling", "Imp", "Ghoul", "Zombie", "Ghost"]
+var randomFriend = ["Pixie", "Faerie"]
+var userHealth = 100
+var help = ("Walk - Push 'W' to walk\n")
+var quit = true
 
-while (userHealth != 0) {
+console.log("\nWELCOME TO BRAN CASTLE, HOME OF COUNT DRACULA\n\nPress enter to begin playing. Press control C at any time to quit.");
 
-    var inventory = []
-    var randomEnemy = ["Werewolf", "Vampire Underling", "Imp", "Ghoul", "Zombie", "Ghost"]
-    var randomFriend = ["Pixie", "Faerie"]
-    var userHealth = 100
-    var help = ("Walk - Push 'W' to walk\n")
-    var key = readline.keyIn();
+readline.prompt();
+var userName = readline.question("\nMuahaha.. Good luck, you will need it.\n\nWhat is your name traveler? ")
+console.log("\nGreetings, " + userName + ". You begin your journey with " + userHealth + " HP.\nTry not to die.\n")
 
-
-    console.log("\nWELCOME TO BRAN CASTLE, HOME OF COUNT DRACULA\n\nPress enter to begin playing. Press control C at any time to quit.");
-
-    readline.prompt();
-    var userName = readline.question("\nMuahaha.. Good luck, you will need it.\n\nWhat is your name traveler? ")
-    console.log("\nGreetings, " + userName + ". You begin your journey with " + userHealth + " HP.\nTry not to die.\n")
+while (userHealth != 0 || quit = true ) {
 
     var key = readline.keyIn();
     switch (key) {
-    case w:
+    case "w":
         console.log("Walking")
+        break;
+    case "q":
+        quit = true;
+        console.log("QUITTER")
+        break;
     default:
-        console.log("any other key")
+        console.log("What else would you like to do?")
+        var menuIndex = readline.keyInSelect(menu, "Select which function you would like to access")
+        break;
+
 
 
     }
 }
 
-
+if (userHealth != 0) {
 console.log("You DIED")
+} else {
+    console.log("Thanks for playing")
+}
